6280d466ed9845c1a026f07a2510d3cdfce101fb
JY <jitengjiao@bytedance.com>
2024-01-29 11:50:07 +0800

    new project
    new project

---
+++ next-doc/src/components/Navbar/config.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: JY jitengjiao@bytedance.com
+ * @Date: 2024-01-28 17:20:44
+ * @LastEditors: JY jitengjiao@bytedance.com
+ * @LastEditTime: 2024-01-28 17:22:05
+ * @FilePath: /next-doc/src/components/Navbar/config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const navs = [{
+    label: "首页",
+    value:"/"
+},{
+    label: "咨询",
+    value:"/info"
+},{
+    label: "标签",
+    value:"/tag"
+}]
--- next-doc/src/components/Navbar/index.tsx
+++ next-doc/src/components/Navbar/index.tsx
@@ -2,14 +2,30 @@
  * @Author: JY jitengjiao@bytedance.com
  * @Date: 2024-01-28 16:52:45
  * @LastEditors: JY jitengjiao@bytedance.com
- * @LastEditTime: 2024-01-28 16:56:05
+ * @LastEditTime: 2024-01-28 18:17:06
  * @FilePath: /next-doc/src/components/Navbar/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { NextPage } from 'next';
+import { NextPage } from 'next'
+import { navs } from './config'
+import Link from 'next/link'
+import styles from './index.module.scss'
 
 const Navbar: NextPage = () => {
-  return <div>导航</div>;
-};
+  return (
+    <div className={styles.navbar}>
+      <section className={styles.logArea}>BLOG-C</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => {
+          return (
+            <Link key={nav?.label} href={nav?.value} legacyBehavior>
+              <a>{nav?.label}</a>
+            </Link>
+          )
+        })}
+      </section>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
